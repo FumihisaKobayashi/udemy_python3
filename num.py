13620500874c9fcf2a2994557ff6a261098ae27f
f.kobayashi <ruruku717@gmail.com>
2021-08-25 23:59:12 +0900

    Math

---
+++ num.py
@@ -0,0 +1,28 @@
+print(2 + 2)
+print(2 * 2)
+print(2 - 2)
+print(2 / 2)
+
+print((50-5) * 3)
+
+print(17 // 3) #整数のみ
+print(17 % 3)#余りのみ
+
+print(2 ** 3)#乗数
+
+#代入式でもできる
+x = 5
+y = 10
+print(x * y)
+
+pie = 3.14151692
+r = round(pie, 2) #round 桁数で丸めて表記
+print(r)
+
+import math #mathをインポートすると数式使える
+
+resuit = math.sqrt(25)
+print(resuit)
+
+#print(help(math))でhelp見る
+
