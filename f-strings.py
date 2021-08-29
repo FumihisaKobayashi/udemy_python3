967fbe02191a440d0ec7a392ed0e4108a62a132e
f.kobayashi <ruruku717@gmail.com>
2021-08-29 16:57:22 +0900

    f-strings

---
+++ f-strings.py
@@ -0,0 +1,14 @@
+a = 'a'
+print(f'a is {a}')
+
+
+x, y, z = 1, 2, 3
+print(f'a is{x}, {y}, {z}')
+print(f'a is{z}, {y}, {z}, {x}')
+
+name = 'Taro'
+family = 'Yamada'
+print(f'Myname is {name} {family}. Watashi ha {family} {name}')
+
+#文字代入 f'a is{test}'で代入
+
