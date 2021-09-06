d059c557245a8b7d8feb8cae7a9368b4dd56af83
f.kobayashi <ruruku717@gmail.com>
2021-09-07 00:51:33 +0900

    集合型メソッド

---
+++ data_structure/collective_method.py
@@ -0,0 +1,16 @@
+s = {1, 2, 3, 4, 5}
+print(s)
+#s[0]で追加データアサインできない
+s.add(6) #s.add()で追加できる
+print(s)
+
+s.remove(6) #remove(6)で(数字)で消す
+print(s)
+s.clear()#中身全てからにする
+
+a = {}
+print(type(a)) #辞書型
+print(a)
+
+#help(set)でメソッド確認できる
+
