5c4964c9b9ca9b5689ed7d7711d04047e1599fb1
f.kobayashi <ruruku717@gmail.com>
2021-09-07 00:35:01 +0900

    辞書使い所

--- data_structure/dictionary_using.py
+++ data_structure/dictionary_using.py
@@ -4,4 +4,13 @@ fruit = {
   'banana': 100,
   'orange': 300,
 }
-print(fruit['apple'])
+print(fruit['apple'])#appleの値段表記
+
+
+#リストでもできるが、appleを探して、値段はどう七日のプログラムを区必要がある
+l = [['apple', 100],
+    ['banana', 100],
+    ['orange', 100]
+]
+#リストは検索に時間がかかる
+#辞書はキーで取り出せる
