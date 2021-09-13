f41389e4c724c79f52843c9b6c91ed94dc0780de
f.kobayashi <ruruku717@gmail.com>
2021-09-13 23:57:05 +0900

    forループ

---
+++ control_structure/for.py
@@ -0,0 +1,12 @@
+some_list = [1, 2, 3, 4, 5]
+
+"""
+i = 0
+while i < len(some_list): #中身を順に出力
+  print(some_list[i])#some_listのi番目
+  i += 1
+"""
+
+for i in some_list: #some_listから（反復処理）inで入れていき、イテレーターをinでいれていく
+  print(i)
+#自分で１ずつ足していく必要がない
