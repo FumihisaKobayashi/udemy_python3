7eb675d7f5da1a4a5d4f4f1b3b0726f60d656240
f.kobayashi <ruruku717@gmail.com>
2021-09-14 00:02:39 +0900

    forループ

--- control_structure/for.py
+++ control_structure/for.py
@@ -6,7 +6,16 @@ while i < len(some_list): #中身を順に出力
   print(some_list[i])#some_listのi番目
   i += 1
 """
-
+"""
 for i in some_list: #some_listから（反復処理）inで入れていき、イテレーターをinでいれていく
   print(i)
 #自分で１ずつ足していく必要がない
+
+for s in 'abcde':#文字abcdeから順に取り出す
+  print(s)
+"""
+
+for word in ['my', 'name', 'is', 'Mike']:
+  if word == 'name':
+      continue #break ループを抜ける #スキップしてループ続ける
+  print(word)
