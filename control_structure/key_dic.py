9b7ef419c2c5fe3d14f891428fa3ab6323da1e4a
f.kobayashi <ruruku717@gmail.com>
2021-09-21 00:02:28 +0900

    キーワード因数の辞書化

---
+++ control_structure/key_dic.py
@@ -0,0 +1,12 @@
+def menu(**kwargs):
+  print(kwargs)
+  for k, v in kwargs.items():
+    print(k,v)
+
+menu(entree='beef', drink='coffee')
+
+d = {
+  'entree':'beef', 
+  'drink': 'ice coffee',
+  'dessert':'ice'
+}
