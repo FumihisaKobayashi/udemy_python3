e1303e1abf982b088682e678fee046d0801b2633
f.kobayashi <ruruku717@gmail.com>
2021-09-25 18:28:54 +0900

    練習

--- control_structure/pra.py
+++ control_structure/pra.py
@@ -5,4 +5,10 @@ while i < len(some_list):
   print(some_list[i])
   i += 1
 
-  
+for fruit in ['apple', 'orange', 'banana']:
+  if fruit == 'orange':
+    print('stop eating!')
+    break #ループから抜ける
+  print(fruit)
+else:#全てのループ終了出力
+  print('I ate all!')
