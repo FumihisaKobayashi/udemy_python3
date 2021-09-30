88aae0c6277a1957f7607361c4f4dbf36c7ebf8c
f.kobayashi <ruruku717@gmail.com>
2021-09-30 23:39:09 +0900

    docstrings

---
+++ control_structure/docstrings.py
@@ -0,0 +1,16 @@
+def example(param1, param2):
+  """[summary]
+
+  Args:
+      param1 ([type]): [description]
+      param2 ([type]): [description]
+
+  Returns:
+      [type]: [description]
+  """
+
+  print(param1)
+  print(param2)
+  return True
+
+print(example.__doc__)
